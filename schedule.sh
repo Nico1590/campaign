6839019f928092eaf5a9737d79070d50f5b65a21
Nicolas <nicolaslievin@hotmail.fr>
2017-03-06 08:15:58 -0500

    auto-commit

--- schedule.sh
+++ schedule.sh
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
 git checkout autoSave
 git add .
 git commit -m "auto-commit"
 git push origin autoSave
 
-=======
-#!/bin/sh
-cd C:/git/campaign
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-
-git checkout 
-git add .
-git commit -m "auto-commit $(timestamp)"
-git push origin autoSave
-# echo Press Enter...
-# read
->>>>>>> 0860477bb08e03dc8837dda7f21f330c7a17c128
